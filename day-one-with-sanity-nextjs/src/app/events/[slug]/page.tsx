665fa401a579f9ceb68bd7b751a01778e996c1b6
toskasogody <toska.shala@sogody.com>
2024-08-12 16:56:50 +0200

    Daily exercise

--- day-one-with-sanity-nextjs/src/app/events/[slug]/page.tsx
+++ day-one-with-sanity-nextjs/src/app/events/[slug]/page.tsx
@@ -1,21 +1,23 @@
-import { PortableText, type SanityDocument } from "next-sanity";
+// From the Next.js code example
+import { PortableText } from "@portabletext/react";
+import { SanityDocument } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
+
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
-import { client, sanityFetch } from "@/sanity/client";
+import { client } from "@/sanity/client";
 import Link from "next/link";
-import Image from "next/image";
 
 const EVENT_QUERY = `*[
-  _type == "event" &&
-  slug.current == $slug
-][0]{
-	...,
-	headline->,
-	venue->
+    _type == "event" &&
+    slug.current == $slug
+  ][0]{
+  ...,
+  headline->,
+  venue->
 }`;
 
 const { projectId, dataset } = client.config();
-const urlFor = (source: SanityImageSource) =>
+export const urlFor = (source: SanityImageSource) =>
   projectId && dataset
     ? imageUrlBuilder({ projectId, dataset }).image(source)
     : null;
@@ -25,12 +27,7 @@ export default async function EventPage({
 }: {
   params: { slug: string };
 }) {
-  const event = await sanityFetch<SanityDocument>({
-    query: EVENT_QUERY,
-    params,
-  });
-  console.log(event); // Add this line to debug
-
+  const event = await client.fetch<SanityDocument>(EVENT_QUERY, params);
   const {
     name,
     date,
@@ -42,9 +39,9 @@ export default async function EventPage({
     venue,
     tickets,
   } = event;
-
-  const eventImageUrl = image
-    ? urlFor(image)?.width(550).height(310).url()
+  const eventImageUrl = image?.asset ? urlFor(image)?.url() : null;
+  const artistImageUrl = headline?.photo?.asset
+    ? urlFor(headline.photo)?.url()
     : null;
   const eventDate = new Date(date).toDateString();
   const eventTime = new Date(date).toLocaleTimeString();
@@ -53,79 +50,99 @@ export default async function EventPage({
   ).toLocaleTimeString();
 
   return (
-    <main className="container mx-auto grid gap-12 p-12">
-      <div className="mb-4">
-        <Link href="/">← Back to events</Link>
-      </div>
-      <div className="grid items-top gap-12 sm:grid-cols-2">
-        <Image
-          src={eventImageUrl || "https://via.placeholder.com/550x310"}
-          alt={name || "Event"}
-          className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full"
-          height="310"
-          width="550"
-        />
-        <div className="flex flex-col justify-center space-y-4">
-          <div className="space-y-4">
-            {format ? (
-              <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm dark:bg-gray-800 capitalize">
-                {format.replace("-", " ")}
-              </div>
-            ) : null}
-            {name ? (
-              <h1 className="text-4xl font-bold tracking-tighter mb-8">
-                {name}
-              </h1>
-            ) : null}
-            {headline?.name ? (
-              <dl className="grid grid-cols-2 gap-1 text-sm font-medium sm:gap-2 lg:text-base">
-                <dd className="font-semibold">Artist</dd>
-                <dt>{headline?.name}</dt>
-              </dl>
-            ) : null}
-            <dl className="grid grid-cols-2 gap-1 text-sm font-medium sm:gap-2 lg:text-base">
-              <dd className="font-semibold">Date</dd>
-              <div>
-                {eventDate && <dt>{eventDate}</dt>}
-                {eventTime && <dt>{eventTime}</dt>}
-              </div>
-            </dl>
-            {doorsOpenTime ? (
-              <dl className="grid grid-cols-2 gap-1 text-sm font-medium sm:gap-2 lg:text-base">
-                <dd className="font-semibold">Doors Open</dd>
-                <div className="grid gap-1">
-                  <dt>Doors Open</dt>
-                  <dt>{doorsOpenTime}</dt>
+    <main className="w-full min-h-screen py-12 md:py-24 lg:py-32">
+      <div className="container px-4 md:px-6 mx-auto">
+        <div className="mb-4">
+          <Link href="/">← Back to events</Link>
+        </div>
+        <div className="grid items-top gap-6 lg:grid-cols-[1fr_500px] lg:gap-12 xl:grid-cols-[1fr_550px]">
+          {(eventImageUrl || artistImageUrl) && (
+            // eslint-disable-next-line @next/next/no-img-element
+            <img
+              alt="Image"
+              className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full"
+              height="310"
+              src={eventImageUrl || artistImageUrl || ""}
+              width="550"
+            />
+          )}
+          <div className="flex flex-col justify-center space-y-4">
+            <div className="space-y-2">
+              {format && (
+                <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm dark:bg-gray-800 capitalize">
+                  {format.replace("-", " ")}
                 </div>
-              </dl>
-            ) : null}
-            {venue?.name ? (
+              )}
+              {name && (
+                <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  {name}
+                </h1>
+              )}
+              {headline?.name && (
+                <dl className="grid grid-cols-2 gap-1 text-sm font-medium sm:gap-2 lg:text-base">
+                  <div className="flex items-start">
+                    <dt className="sr-only">Artist</dt>
+                    <dd className="font-semibold">Artist</dd>
+                  </div>
+                  <div className="grid gap-1">
+                    <dt>{headline?.name}</dt>
+                  </div>
+                </dl>
+              )}
               <dl className="grid grid-cols-2 gap-1 text-sm font-medium sm:gap-2 lg:text-base">
                 <div className="flex items-start">
+                  <dt className="sr-only">Date</dt>
+                  <dd className="font-semibold">Date</dd>
+                </div>
+                <div className="grid gap-1">
+                  {eventDate && <dt>{eventDate}</dt>}
+                  {eventTime && <dt>{eventTime}</dt>}
+                </div>
+              </dl>
+              {doorsOpenTime && (
+                <dl className="grid grid-cols-2 gap-1 text-sm font-medium sm:gap-2 lg:text-base">
+                  <div className="flex items-start">
+                    <dt className="sr-only">Doors Open</dt>
+                    <dd className="font-semibold">Doors Open</dd>
+                  </div>
+                  <div className="grid gap-1">
+                    <dt>Doors Open</dt>
+                    <dt>{doorsOpenTime}</dt>
+                  </div>
+                </dl>
+              )}
+              <dl className="grid grid-cols-2 gap-1 text-sm font-medium sm:gap-2 lg:text-base">
+                <div className="flex items-start">
+                  <dt className="sr-only">Venue</dt>
                   <dd className="font-semibold">Venue</dd>
                 </div>
                 <div className="grid gap-1">
                   <dt>{venue.name}</dt>
+                  <dt>
+                    {venue.city}, {venue.country}
+                  </dt>
                 </div>
               </dl>
-            ) : null}
-          </div>
-          {details && details.length > 0 && (
-            <div className="prose max-w-none">
-              <PortableText value={details} />
             </div>
-          )}
-          {tickets && (
-            <a
-              className="flex items-center justify-center rounded-md bg-blue-500 p-4 text-white"
-              href={tickets}
-            >
-              Buy Tickets
-            </a>
-          )}
-          
+            {details && details.length > 0 && (
+              <div className="prose max-w-none">
+                <PortableText value={details} />
+              </div>
+            )}
+            {tickets && (
+              <div className="flex gap-4">
+                <a
+                  className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 w-1/2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+                  href={tickets}
+                >
+                  Buy Tickets
+                </a>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </main>
   );
 }
+
--- day-one-with-sanity-nextjs/src/app/page.tsx
+++ day-one-with-sanity-nextjs/src/app/page.tsx
@@ -1,36 +1,33 @@
 import Link from "next/link";
-import { SanityDocument } from "next-sanity";
+import { groq } from "next-sanity";
 
 import { sanityFetch } from "@/sanity/client";
+import { EVENTS_QUERYResult } from "@/sanity/types";
 
-const EVENTS_QUERY = `*[
-  _type == "event"
-  && defined(slug.current)
-]{_id, name, slug, date}|order(date desc)`;
+const EVENTS_QUERY = groq`*[_type == "event"]{_id, name, slug, date}|order(date desc)`;
 
 export default async function IndexPage() {
-  const events = await sanityFetch<SanityDocument[]>({query: EVENTS_QUERY});
+  const events = await sanityFetch<EVENTS_QUERYResult>({ query: EVENTS_QUERY });
 
   return (
     <main className="flex bg-gray-100 min-h-screen flex-col p-24 gap-12">
-      <h1 className="text-4xl font-bold tracking-tighter">
-        Events
-      </h1>
+      <h1 className="text-4xl font-bold tracking-tighter">Events</h1>
       <ul className="grid grid-cols-1 gap-12 lg:grid-cols-2">
         {events.map((event) => (
-          <li
-            className="bg-white p-4 rounded-lg"
-            key={event._id}
-          >
-            <Link
-              className="hover:underline"
-              href={`/events/${event.slug.current}`}
-            >
-              <h2 className="text-xl font-semibold">{event?.name}</h2>
-              <p className="text-gray-500">
-                {new Date(event?.date).toLocaleDateString()}
-              </p>
-            </Link>
+          <li className="bg-white p-4 rounded-lg" key={event._id}>
+            {event.slug?.current ? (
+              <Link
+                className="hover:underline"
+                href={`/events/${event.slug.current}`}
+              >
+                <h2 className="text-xl font-semibold">{event?.name}</h2>
+                {event?.date ? (
+                  <p className="text-gray-500">
+                    {new Date(event.date).toLocaleDateString()}
+                  </p>
+                ) : null}
+              </Link>
+            ) : null}
           </li>
         ))}
       </ul>
